72a3cfaaa0e396ec59c29b8caa574faa45e6889a
Gabriel Carneiro <gabriel29music@gmail.com>
2021-12-02 10:22:47 -0300

    get leads implementado

--- controllers/leads.js
+++ controllers/leads.js
@@ -1,6 +1,15 @@
+const knex = require("../databaseConnection")
 
 const getLeads = async (req, res) => {
-    return res.json("Tudo certo")
+    const user = req.user
+    //return res.json(user)
+    try {
+        const leads = await knex("leads").where({user_id:user.id});
+        return res.json(leads)
+        
+    } catch (error) {
+        return res.status(400).json({mensagem: error.message})
+    }
 
 }
 
